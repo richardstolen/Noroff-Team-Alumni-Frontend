3c17acddf9150f5f20255775b2051ed1cc9f5509
Jacob Tornes <jacob.emil.tornes@no.experis.com>
2023-03-10 11:00:34 +0100

    :lipstick: update login-page

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import {
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
+      <div className=" d-grid h-100">
         
         <Routes>
           <Route path="/" element={<Login/>}/>
