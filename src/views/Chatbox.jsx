e1326a90acd03306ace2fb08cde979ffc51f0c06
Jacob Tornes <jacob.emil.tornes@no.experis.com>
2023-03-27 13:21:26 +0200

    :adhesive_bandage: corrected a module name

--- src/views/Chatbox.jsx
+++ src/views/Chatbox.jsx
@@ -1,4 +1,4 @@
-import Chat from "../components/Dashboard/Chat";
+import Chat from "../components/Chat/Chat";
 import Navbar1 from "../components/Navbar/Navbar1";
 import CalendarSidebar from "../components/Sidebar/CalendarSidebar";
 import { useState, useEffect } from "react";
