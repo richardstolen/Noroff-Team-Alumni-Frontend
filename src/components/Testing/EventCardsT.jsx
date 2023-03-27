acaa7fea2cf212042dfdf4a98aa27c7798bd95ba
Kjetil Rønhovde <kjetil.ronhovde@no.experis.com>
2023-03-27 14:29:34 +0000

    :sparkles: create join and leave button for event

--- src/components/Testing/EventCardsT.jsx
+++ src/components/Testing/EventCardsT.jsx
@@ -1,28 +1,93 @@
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-import { joinEvent } from "../../api/eventApi";
-import { getEventbyId, getEventByUser, createEvent } from "../../api/eventApi";
+import { getEventByUser, joinEvent } from "../../api/eventApi";
+import KeyCloakService from "../../security/KeyCloakService.ts";
+import { getUser} from "../../api/apiHandler";
+import { useEffect, useState } from "react";
+import EventAPI from "../../api/eventApi";
+import Storage from "../../storage/storage";
+
+
+
+
+const fetchData = async () => {
+  const data = await getUser(KeyCloakService.GetId());
+  const allEvent = await getEventByUser();
+  const userWithEvents = {
+    ...data,
+    events: allEvent
+  };
+  return userWithEvents;
+};
 
 function EventCards(event) {
+  const [user, setUser] = useState(Storage.getUser());
+
+  useEffect(() => {
+    let userFromStorage = Storage.getUser();
+    if (!userFromStorage) {
+      fetchData().then((user) => {
+        setUser(user);
+        Storage.setUser(user);
+      });
+    } else {
+      Storage.setUser(userFromStorage);
+    }
+  }, [user, setUser]);
+
   const handleJoinEvent = async () => {
+    const result = await joinEvent(event.prop.eventId).then(
+      (response) => response
+    );
+
+    
+
+    if (result.ok) {
+      fetchData().then((user) => {
+        setUser(user);
+        Storage.setUser(user);
+      });
+    }
+  };
+
+
+
+  const handleLeaveEvent = async () => {
     try {
-      const result = await joinEvent(event.prop.eventId);
-      console.log("success", result);
+      const result = await EventAPI.leaveEvent(event.prop.eventId).then(
+        (response) => response
+      );
+
+      if (result.ok) {
+        fetchData().then((user) => {
+          setUser(user);
+          Storage.setUser(user);
+        });
+      }
     } catch (error) {
       console.log("error", error);
     }
   };
+
   return (
     <div style={{ display: "flex", justifyContent: "center" }} className="mt-5">
+      {event?.prop&& (
       <Card style={{ width: "18rem", margin: "1rem" }}>
         <Card.Body>
-          {/* <Card.Text>{event.prop.title}</Card.Text> */}
           <Card.Text>{event.prop.description}</Card.Text>
-          <Button variant="primary" onClick={() => handleJoinEvent(event.prop.description)}>
-            Leave event
+          {console.log(user)}
+          {!user.events.some((x) => x.eventId === event.prop.eventId) ? (
+          <Button variant="primary" onClick={() => handleJoinEvent(event.prop)}>
+            Join event
           </Button>
+          ) : (
+            <Button variant="primary" onClick={() => handleLeaveEvent(event.prop)}>
+              Leave event
+            </Button>
+          )}
         </Card.Body>
       </Card>
+  )}
     </div>
   );
 }
